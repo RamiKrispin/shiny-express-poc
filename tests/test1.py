657ae431a7f45595b05de2195e9e358f48695045
RamiKrispin <ramkrisp@umich.edu>
2024-02-16 20:35:59 -0800

    update the test

--- tests/test1.py
+++ tests/test1.py
@@ -1,3 +1,8 @@
-import pandas as pd 
+import faicons as fa
+import plotly.express as px
+from shinywidgets import render_plotly
+
+from shiny import reactive, render, req
+from shiny.express import input, ui
 
 print("Hello World!")
